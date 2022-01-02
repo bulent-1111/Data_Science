fa3de7d464f0613ac6b1242e2902c70485960b6f
bulent-1111 <akuklubulent@gmail.com>
2022-01-02 21:26:04 +0100

    created db with python

--- SQL_With_Python/sql_1.py
+++ SQL_With_Python/sql_1.py
@@ -1,8 +1,8 @@
 import os,sys,sqlite3
-if os.path.exists("firma.db"):
+if os.path.exists("firma1.db"):
     print("Datei bereits vorhanden")
     sys.exit(0)
-connection = sqlite3.connect("firma.db")
+connection = sqlite3.connect("firma1.db")
 cursor  = connection.cursor()
 sql ="CREATE TABLE personen("\
     "name TEXT, "\
@@ -11,9 +11,21 @@ sql ="CREATE TABLE personen("\
     "gehalt FLOAT, "\
     "geburtstag TEXT )"
 cursor.execute(sql)
-sql = "INSERT INTO personen VALUES('Maler', "\
-    "'Hnas',6714,3500,'15.03.1962')"
+sql = "INSERT INTO personen VALUES('Taller', "\
+    "'Mans',6000,2500,'10.01.1970')"
 cursor.execute(sql)
 connection.commit()
+
+sql = "INSERT INTO personen VALUES('Schmidt', "\
+    "'Peter',86714,4500,'25.04.1958')"
+cursor.execute(sql)
+connection.commit()
+
+sql = "INSERT INTO personen VALUES('Bayer', "\
+    "'Julia',2287,3750,'30.12.1959')"
+cursor.execute(sql)
+connection.commit()
+
+
 connection.close()
 
