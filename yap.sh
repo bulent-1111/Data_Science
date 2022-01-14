32f9077a025c8d9ddf3fca5e9f046bb2acc5a8ba
bulent-1111 <akuklubulent@gmail.com>
2022-01-14 22:39:27 +0100

    pandas Series code added

--- yap.sh
+++ yap.sh
@@ -2,7 +2,7 @@ git add .
 echo " 'git add . ' yapildi"
 echo "Dosyalar eklendi."
 echo "........................."
-git commit -m  "pandas code added"
+git commit -m  "pandas Series code added"
 echo "commit yapildi"
 echo "........................."
 git push
