fcb1bb66419f0c7548467f095da52f8c8cd59476
bulent-1111 <akuklubulent@gmail.com>
2022-01-14 23:42:27 +0100

    pandas Read JSON code added

--- yap.sh
+++ yap.sh
@@ -2,7 +2,7 @@ git add .
 echo " 'git add . ' yapildi"
 echo "Dosyalar eklendi."
 echo "........................."
-git commit -m  "pandas Read CSV code added"
+git commit -m  "pandas Read JSON code added"
 echo "commit yapildi"
 echo "........................."
 git push
