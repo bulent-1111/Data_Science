e4fc35178e109e7b890f49be1b2e469af56bfd54
bulent-1111 <akuklubulent@gmail.com>
2022-01-19 00:27:00 +0100

    Pandas code added

--- yap.sh
+++ yap.sh
@@ -2,7 +2,7 @@ git add .
 #echo -e "\033[44m\033[37m.........................\033[0m"
 echo -e "\033[44m\033[37m.....git add . yapildi...\033[0m"
 #echo -e "\033[44m\033[37m.........................\033[0m"
-git commit -m "python code added"
+git commit -m "Pandas code added"
 #echo -e "\033[44m\033[37m.........................\033[0m"
 echo -e "\033[44m\033[37m.....commit yapildi......\033[0m"
 #echo -e "\033[44m\033[37m.........................\033[0m"
