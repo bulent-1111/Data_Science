885ffa78acfb7e932058acd75800a01c2803e5f4
bulent-1111 <akuklubulent@gmail.com>
2022-01-30 15:27:33 +0100

    Python Data Visualization code added

--- yap.sh
+++ yap.sh
@@ -5,7 +5,7 @@ git add .
 echo -e "\033[44m\033[37m                         \033[0m"
 echo -e "\033[44m\033[37m.....git add . yapildi...\033[0m"
 #echo -e "\033[44m\033[37m.........................\033[0m"
-git commit -m "Python Matplotlib code added"
+git commit -m "Python Data Visualization code added"
 #echo -e "\033[44m\033[37m.........................\033[0m"
 echo -e "\033[44m\033[37m.....commit yapildi......\033[0m"
 #echo -e "\033[44m\033[37m.........................\033[0m"
